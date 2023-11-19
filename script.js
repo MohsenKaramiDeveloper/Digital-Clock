94ce40d382b674560527bfcdfe82984ac6b316d7
Mohsen Karami <kara0158@algonquinlive.com>
2023-11-18 21:54:54 -0500

    delete last change

--- script.js
+++ script.js
@@ -20,9 +20,7 @@ function changeColor(event){
 
 
 
-let hrs = document.getElementById("hrs")
-let min = document.getElementById("min")
-let sec = document.getElementById("sec")
+
 
 setInterval(()=>{
     let currentTime = new Date();
