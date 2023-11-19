ec495a33e65aadc1364d377f0c56a331fe0f1f38
Mohsen Karami <kara0158@algonquinlive.com>
2023-11-18 22:34:57 -0500

    -a1

--- script.js
+++ script.js
@@ -19,18 +19,3 @@ function changeColor(event){
 }
 
 
-setInterval(()=>{
-    let currentTime = new Date();
-
-hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
-min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
-sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
-
-
-},1000)
-
-
-var text = document.getElementById('text');
-function changeColor(event){
-    text.style.color=event.target.value;
-}
