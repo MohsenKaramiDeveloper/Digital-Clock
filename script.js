fc88752912cc370f6980dacb919072402480a199
Mohsen Karami <kara0158@algonquinlive.com>
2023-11-19 23:26:26 -0500

    re do fase 1

--- script.js
+++ script.js
@@ -20,5 +20,23 @@ function changeColor(event){
 
 
 
+//let hrs = document.getElementById("hrs")
+let min = document.getElementById("min")
+let sec = document.getElementById("sec")
 
+setInterval(()=>{
+    let currentTime = new Date();
+
+hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
+min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
+sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
+
+
+},1000)
+
+
+var text = document.getElementById('text');
+function changeColor(event){
+    text.style.color=event.target.value;
+}
 
